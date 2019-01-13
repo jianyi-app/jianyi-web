3649287d2c167984bec7e02dfd781985957c6463
yangshiyuan <yangshiyuan@outlook.com>
2019-01-13 18:41:52 +0800

    first commit

---
+++ src/main/java/com/jianyi/web/controller/DiscoveryController.java
@@ -0,0 +1,31 @@
+package com.jianyi.web.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class DiscoveryController {
+
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
+    @RequestMapping("/discovery")
+    public Object discovery() {
+        List<String> list = discoveryClient.getServices();
+        System.out.println("**********" + list);
+
+        List<ServiceInstance> srvList = discoveryClient.getInstances("JIANYI-USER");
+        for (ServiceInstance element : srvList) {
+            System.out.println(element.getServiceId() + "\t" + element.getHost() + "\t" + element.getPort() + "\t"
+                    + element.getUri());
+        }
+        return list;
+
+    }
+
+}
